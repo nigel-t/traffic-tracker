17a6a1eeaa2c4024e53a1942d3bca70102bb6905
nigel-t <nigeltunnacliffe@lc02j62liffemba.attlocal.net>
2014-11-11 16:35:44 -0800

    trimming the fat

--- common-files/js/script.js
+++ common-files/js/script.js
@@ -32,7 +32,7 @@ function fadedEls(el, shift) {
     $(function() {
         var videobackground = new $.backgroundVideo($('#bgVideo'), {
             "align" : "centerXY",
-            "path" : "video/",
+            "path" : "common-files/video/",
             "width": 1280,
             "height": 720,
             "filename" : "traffic-405",
